f6ea61f46b4fd50cdeda4380feebfdbf5a047582
John de Kroon <John.deKroon@ortec-finance.com>
2015-06-02 19:57:29 +0200

    set-up of first program

---
+++ NeuralNetworks2/NeuralNetworks2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworks2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nBegin perceptron demo:\n");
+            Console.WriteLine("Predict liberal (-1) or conservative (1) from age, incom");
+            //create perceptron program 
+            Console.WriteLine("\nEnd perceptron demo\n");
+            Console.ReadLine();
+        }
+
+        private static void ShowData(double[][] trainingData)
+        { }
+
+        static void ShowVector(double[] vector, int decimals, bool newline)
+        { }
+    }
+
+    public class Perceptron
+    {
+        //fields and methods are defined here
+    }
+}
